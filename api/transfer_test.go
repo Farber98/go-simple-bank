005c4614ffecb2edadaab3c8bfcf78577a4eb9ad
Farber98 <juantuc98@gmail.com>
2022-06-13 09:35:43 -0300

    added OK test for Tx API.

---
+++ api/transfer_test.go
@@ -0,0 +1,88 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	mockdb "go-simple-bank/db/mock"
+	db "go-simple-bank/db/sqlc"
+	"go-simple-bank/util"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateTxAPI(t *testing.T) {
+	account1 := randomAccount()
+	account2 := randomAccount()
+	account3 := randomAccount()
+
+	account1.Currency = util.ARS
+	account2.Currency = util.ARS
+	account3.Currency = util.EUR
+
+	amount := int64(10)
+
+	testCases := []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recoder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"from_account_id": account1.ID,
+				"to_account_id":   account2.ID,
+				"amount":          amount,
+				"currency":        util.ARS,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(account1, nil)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account2.ID)).Times(1).Return(account2, nil)
+
+				args := &db.TransferTxParams{
+					FromAccountId: account1.ID,
+					ToAccountId:   account2.ID,
+					Amount:        amount,
+				}
+
+				store.EXPECT().TransferTx(gomock.Any(), *args).Times(1)
+			},
+			checkResponse: func(recoder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recoder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			store := mockdb.NewMockStore(ctrl)
+
+			tc.buildStubs(store)
+
+			// start test server and send request
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/transfers"
+			req, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, req)
+
+			tc.checkResponse(recorder)
+		})
+	}
+
+}
